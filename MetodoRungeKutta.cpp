daefb98bd310635740fbf782d3512db7152b3460
ins <ins@cin.ufpe.br>
2017-06-17 22:33:52 -0300

    Runge Kutta aplicado ao foguete, falta verificar se esta realmente correto

--- MetodoRungeKutta.cpp
+++ MetodoRungeKutta.cpp
@@ -13,36 +13,39 @@
 using namespace std;
 
 
-double funcao (double t, double q){
-
-	return (-0.5*q + 10 + sin(2*t));
-
+float derivada(float massa, float tempo) {
+	float dv, dm, u;
+	dm = 10.f;
+	u = 100.f;
+	if (massa > dm) {
+		dv = u*dm/massa;
+	} else dv = 0.f;
+	//cout<<dv<<endl;
+	return dv;
 }
+
 
 int main(){
 	
 	int n, i;
-	double x, y, h, k1, k2, k3, k4, k;
+	float x, y, h, k1, k2, k3, k4, mi, mf, dm;
 
-	cout<<"Qual a quantidade do numero de passos?"<<endl;
-	cin>>n;
-	cout<<"Qual o valor inicial de x?"<<endl;
-	cin>>x;
-	k = x;
+	n = 10; //quantidade de interações
+	mi = 100000.f; //massa inicial
+	y = 1000.f; // tempo
+	h = 1.f;
+	dm = 10.f;
+	mf = mi - y*dm;
 	
-	cout<<"Qual o valor inicial de y?"<<endl;
-	cin>>y;
-	cout<<"Qual o espacamento do intervalo?"<<endl;
-	cin>>h;
-	
-	for(i=0; i<n; i++){
+	for(i=0; (i<n) && (mi > mf); i++){
 	    
-	    x=k+i*h;
-	    k1=funcao(x,y);
-	    k2=funcao(x+(h/2),y+(h/2)*k1);
-	    k3=funcao(x+(h/2), y+ (h/2)*k2);
-	    k4=funcao(x+h,y+h*k3);
-	    y=y+(h/6)*(k1+2*k2+2*k3+k4);
+	    x = mi + i*h;
+	    k1 = derivada(x,y);
+	    k2 = derivada(x+(h/2), y+(h/2)*k1);
+	    k3 = derivada(x+(h/2), y+ (h/2)*k2);
+	    k4 = derivada(x+h, y+h*k3);
+	    y = y + (h/6)*(k1+2*k2+2*k3+k4);
+	    mi -= dm;
 	}
 	cout<<"O valor aproximado da funcao para "<<n<<" passos eh "<<y<<endl;
 }
