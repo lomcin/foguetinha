bb730f73ed074ef981049533e7e55a0834f85803
Iraline Nunes Simoes <ins@ug2c20.windows.cin.ufpe.br>
2017-06-20 10:57:43 -0300

    RungeKutta completo

--- MetodoRungeKutta.cpp
+++ MetodoRungeKutta.cpp
@@ -14,38 +14,45 @@ using namespace std;
 
 
 float derivada(float massa, float tempo) {
-	float dv, dm, u;
-	dm = 10.f;
-	u = 100.f;
-	if (massa > dm) {
-		dv = u*dm/massa;
-	} else dv = 0.f;
-	//cout<<dv<<endl;
-	return dv;
+  float dv, dm, u;
+  dm = 10.f;
+  u = 100.f;
+  if (massa > dm) {
+    dv = u*dm/massa;
+  } else dv = 0.f;
+  //cout<<dv<<endl;
+  return dv;
+}
+
+float ResultadoAnalitico(float u, float mi, float mf, float vi) {
+  float vf;
+  vf = u*log(mi/mf) + vi;
+  return vf;
 }
 
 
 int main(){
-	
-	int n, i;
-	float x, y, h, k1, k2, k3, k4, mi, mf, dm;
-
-	n = 10; //quantidade de interações
-	mi = 100000.f; //massa inicial
-	y = 1000.f; // tempo
-	h = 1.f;
-	dm = 10.f;
-	mf = mi - y*dm;
-	
-	for(i=0; (i<n) && (mi > mf); i++){
-	    
-	    x = mi + i*h;
-	    k1 = derivada(x,y);
-	    k2 = derivada(x+(h/2), y+(h/2)*k1);
-	    k3 = derivada(x+(h/2), y+ (h/2)*k2);
-	    k4 = derivada(x+h, y+h*k3);
-	    y = y + (h/6)*(k1+2*k2+2*k3+k4);
-	    mi -= dm;
-	}
-	cout<<"O valor aproximado da funcao para "<<n<<" passos eh "<<y<<endl;
+  
+  int n, i;
+  float x, vi, h, k1, k2, k3, k4, mi, mf, dm;
+  
+  n = 1; //quantidade de interações
+  mi = 100000.f; //massa inicial
+  vi = 100.f; // velocidade inicial
+  h = 1.f;
+  dm = 10.f;
+  mf = mi - vi*dm;
+  cout<<"O resultado analitico eh" << ResultadoAnalitico(100.f, mi, mf, vi)<<endl;
+  for(i=0; (i<n) && (mi > mf); i++){
+    
+    x = mi + i*h;
+    k1 = derivada(x,vi);
+    k2 = derivada(x+(h/2), vi +(h/2)*k1);
+    k3 = derivada(x+(h/2), vi + (h/2)*k2);
+    k4 = derivada(x+h, vi+h*k3);
+    vi = vi + (h/6)*(k1+2*k2+2*k3+k4);
+    mi -= dm;
+  }
+  cout<<"O valor aproximado da funcao para "<<n<<" passos eh "<<vi<<endl;
+  
 }
